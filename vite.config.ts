b7f3d2d2d49606e79d9a71860c346d1aee48bc26
Chanthorn Yoeun <44230728+chanthornyoeun@users.noreply.github.com>
2025-11-26 19:10:34 +0700

    Change base URL for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://chanthornyoeun.github.io/portfolio_v1/',
+  base: '/portfolio_v1/',
 })
